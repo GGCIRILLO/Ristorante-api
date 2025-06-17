aef992cdbf8f08b28289081d75c5e66a41606892
Luigi Cirillo <luigicirillo23@gmail.com>
2025-06-17 19:53:54 +0200

    Remove AddBevandaToOrdine method from PietanzaRepository to streamline order management functionality.

--- api/routes.go
+++ api/routes.go
@@ -104,7 +104,6 @@ func SetupRoutes(db *database.DB) *chi.Mux {
 			r.Delete("/{id}", pietanzaHandler.DeletePietanza)
 			r.Post("/ordine/{id_ordine}", pietanzaHandler.AddPietanzaToOrdine)
 			r.Post("/menu-fisso/ordine/{id_ordine}", pietanzaHandler.AddMenuFissoToOrdine)
-			r.Post("/ordine/{id_ordine}/bevanda", pietanzaHandler.AddBevandaToOrdine)
 		})
 
 		r.Route("/menu-fissi", func(r chi.Router) {
